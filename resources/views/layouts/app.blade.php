36b049de168f5e782a903eaa1e4ab97bae65d5d6
caffi <caffi.senna@gmail.com>
2024-04-14 11:02:39 +0900

    delete msg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,13 +101,13 @@
 
         <!-- Main Footer -->
         <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
+            {{-- <div class="float-right d-none d-sm-block">
                 <b>Version</b> 3.1.0
             </div>
             <strong>
                 Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.
             </strong>
-            All rights reserved.
+            All rights reserved. --}}
         </footer>
     </div>
 
