9ef654fed8dcfb173e078f7d77cfdb86801d13c7
caffi <caffi.senna@gmail.com>
2024-04-14 10:11:39 +0900

    small fix

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h2><span uk-icon="icon: download"></span>ダウンロード</h2>
+            <h2>ダウンロード</h2>
         </div>
     </div>
     <ul>
-        <li><a href="{{ asset('download/participants_template.xlsx') }}">参加者一括登録用テンプレート(xlsx形式)</a></li>
+        <li><a href="{{ asset('download/participants_template.xlsx') }}">参加者一括登録用テンプレート(xlsx)</a></li>
     </ul>
 @endsection
