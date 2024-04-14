3e533d25500abe90ee50a77f2609b0baee8dc9be
caffi <caffi.senna@gmail.com>
2024-04-14 22:06:27 +0900

    add template file link

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -7,6 +7,7 @@
         </div>
     </div>
     <p>参加者情報の入力されたエクセルファイルをアップロードして一括登録します。(ファイルサイズ最大2MB)</p>
+    <p class=""><a href="{{ route('download') }}">テンプレートのダウンロード</a></p>
     @include('flash::message')
     <form action="{{ route('upload') }}" method="POST" enctype="multipart/form-data">
         @csrf
