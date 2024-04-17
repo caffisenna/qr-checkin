c366bb4c6e8cc15912aa36faae0ff7772b8d5475
caffi <caffi.senna@gmail.com>
2024-04-17 17:08:20 +0900

    fix

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -8,7 +8,7 @@
 <li class="nav-item">
     <a href="{{ route('participants.index') }}" class="nav-link {{ Request::is('participants*') ? 'active' : '' }}">
         <span uk-icon="icon: users"></span>
-        <p>参加者</p>
+        <p>参加者11</p>
     </a>
 </li>
 
