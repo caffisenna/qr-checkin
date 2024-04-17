797fed7b239569f9a2ecf8c7893d99b20b8e50bc
caffi <caffi.senna@gmail.com>
2024-04-17 17:21:07 +0900

    fix

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -8,7 +8,7 @@
 <li class="nav-item">
     <a href="{{ route('participants.index') }}" class="nav-link {{ Request::is('participants*') ? 'active' : '' }}">
         <span uk-icon="icon: users"></span>
-        <p>参加者11</p>
+        <p>参加者</p>
     </a>
 </li>
 
@@ -16,13 +16,13 @@
 <li class="nav-item">
     <a href="{{ route('download') }}" class="nav-link {{ Request::is('download') ? 'active' : '' }}">
         <span uk-icon="icon: download"></span>
-        <p>ダウンロード12</p>
+        <p>ダウンロード</p>
     </a>
 </li>
 
 <li class="nav-item">
     <a href="{{ route('usage') }}" class="nav-link {{ Request::is('usage') ? 'active' : '' }}">
         <span uk-icon="icon: file-text"></span>
-        <p>使い方ガイド34</p>
+        <p>使い方ガイド</p>
     </a>
 </li>
