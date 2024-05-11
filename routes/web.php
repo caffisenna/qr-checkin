8f2c60e8519de0f45037c35193e25e1ee96d21d3
caffi <caffi.senna@gmail.com>
2024-05-11 21:58:23 +0900

    ログインしていなければtopにredirect Fixes #36

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,26 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// イベント管理
-Route::resource('events', App\Http\Controllers\EventsController::class);
+Route::group(['middleware' => 'ensureLoggedIn'], function () {
+    // チェックイン必須route
 
-// 参加者管理
-Route::resource('participants', App\Http\Controllers\ParticipantsController::class);
+    // イベント管理
+    Route::resource('events', App\Http\Controllers\EventsController::class);
 
-// 参加者アップロードと登録
-// Route::get('/upload', [App\Http\Controllers\ParticipantsController::class, 'showUploadForm'])->name('upload.form');
-Route::get('participants/{event_id}/upload', [App\Http\Controllers\ParticipantsController::class, 'upload_view'])->name('upload_view');
-Route::post('/upload', [App\Http\Controllers\ParticipantsController::class, 'upload'])->name('upload');
+    // 参加者管理
+    Route::resource('participants', App\Http\Controllers\ParticipantsController::class);
 
-// チェックイン
-Route::match(['get', 'post'], '/checkin', [App\Http\Controllers\EventsController::class, 'checkin'])->name('checkin');
+    // 参加者アップロードと登録
+    // Route::get('/upload', [App\Http\Controllers\ParticipantsController::class, 'showUploadForm'])->name('upload.form');
+    Route::get('participants/{event_id}/upload', [App\Http\Controllers\ParticipantsController::class, 'upload_view'])->name('upload_view');
+    Route::post('/upload', [App\Http\Controllers\ParticipantsController::class, 'upload'])->name('upload');
+
+    // チェックイン
+    Route::match(['get', 'post'], '/checkin', [App\Http\Controllers\EventsController::class, 'checkin'])->name('checkin');
+
+    // 参加者export
+    Route::get('/export_members', [App\Http\Controllers\EventsController::class, 'export_members'])->name('export_members');
+});
 
 
 // ダウンロード
@@ -41,6 +48,3 @@ Route::get('/template', [App\Http\Controllers\DownloadController::class, 'downlo
 
 // 使い方
 Route::get('/usage', [App\Http\Controllers\UsageController::class, 'usage'])->name('usage');
-
-// 参加者export
-Route::get('/export_members', [App\Http\Controllers\EventsController::class, 'export_members'])->name('export_members');
